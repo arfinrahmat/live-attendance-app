b6d32797013ca6de388a45e72e17bb2d5b6ec24c
arfinrahmat <59864581+arfinrahmat@users.noreply.github.com>
2021-09-30 15:36:29 +0700

    ui:add login page

---
+++ app/src/main/java/com/arfinrahmat/liveattendanceapp/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.arfinrahmat.liveattendanceapp.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arfinrahmat.liveattendanceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
